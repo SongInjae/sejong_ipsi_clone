a188c80b2225a4865141774f44107110d04f2ac3
SongInJae <pan0663@naver.com>
2023-07-29 15:44:40 +0900

    폴더구조 재변경

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,9 @@ const path = require("path");
 
 module.exports = {
   mode: "development",
-  entry: "./src/index.js",
+  entry: "./public/index.js",
   output: {
-    path: path.resolve(__dirname, "src"),
+    path: path.resolve(__dirname, "public"),
     filename: "index_bundle.js",
   },
   module: {
